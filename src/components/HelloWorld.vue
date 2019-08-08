72edaf18e7ca25aa895f282fd8daef00b29eea68
zhuxueqiang <1317558575@qq.com>
2019-08-08 10:45:38 +0800

    xi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,29 @@
 <template>
-  <div class="hello">
-    <el-slider v-model="value8" show-input>
-    </el-slider>
-  </div>
+<div>
+<p>ssssss</p>
+   <top>
+          <div class="tmpl" slot="up">
+              <span>菜单1</span>
+              <span>菜单2</span>
+              <span>菜单3</span>
+              <span>菜单4</span>
+              <span>菜单5</span>
+              <span>菜单6</span>
+            </div>
+   </top>
+   </div>
 </template>
 
 <script>
+import top  from './coomit/top.vue'
 export default {
-  name: "HelloWorld",
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
-    };
-  }
-};
+   components:{
+      top
+   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
---
+++ src/components/coomit/top.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+   <div>这是头部</div>
+   <slot name="up"></slot>
+    <slot name="up"> </slot>
+</div>
+</template>
+<script>
+
+</script>
